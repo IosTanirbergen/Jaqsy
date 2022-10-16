e3702436232caf386ad524f3145be91c3991a000
iosTanirbergen <iamTanirbergen@gmail.com>
2022-10-16 23:09:17 +0600

    upload main empty project

---
+++ Jaqsy/JaqsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaqsyApp.swift
+//  Jaqsy
+//
+//  Created by Tanir&Almira on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct JaqsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
